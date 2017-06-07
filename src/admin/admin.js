22ad1aeda6c8322708eb97fd03ce1595a223db2c
Dave Kiss <dave@davekiss.com>
2017-06-07 14:48:00 -0400

    Introduce styled-components

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
 const target = document.getElementById('endorsements');
 
-const Metabox = () => {
+/* Create a Title component that'll render an <h1> tag with some styles */
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #0073aa;
+`;
+
+/* Create a Metabox component that'll render a <section> tag with some styles */
+const Metabox = styled.section`
+  padding: 4em;
+  background: #ffffff;
+`;
+
+/* Use Title and Metabox like any other React component – except they're styled! */
+const App = () => {
   return (
-    <div>
-      <h1>Hello from React!</h1>
-    </div>
+    <Metabox>
+      <Title>
+        Hello from React!
+      </Title>
+    </Metabox>
   )
 }
 
 const render = () => {
   ReactDOM.render(
-    <Metabox />,
+    <App />,
     target,
   );
 };
