d5ea5e7f4f32b91367ecd5d84772c0d30f9eef65
Dave Kiss <dave@davekiss.com>
2017-06-15 12:51:48 -0400

    Add a basic state example to the Metabox component

--- lib/app/components/backend/Metabox/Metabox.jsx
+++ lib/app/components/backend/Metabox/Metabox.jsx
@@ -8,23 +8,30 @@ import Button from 'components/backend/Button/Button.jsx';
 const Wrapper = styled.section`
   padding: 4em;
   background: #ffffff;
+  text-align: center;
 `;
 
 class Metabox extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			endorsements: [],
+		};
 		this.handleClick = this.handleClick.bind(this);
 	}
 
 	handleClick(e) {
 		e.preventDefault();
-		console.log('Add a new endorsement here!');
+		const count = this.state.endorsements.length;
+		const endorsements = this.state.endorsements.concat(count + 1);
+		this.setState({endorsements});
 	}
 
 	render() {
 		return (
 			<Wrapper>
 				<Title />
+				<p>This page has {this.state.endorsements.length} endorsements.</p>
 				<Button text="Add New" handler={this.handleClick} />
 			</Wrapper>
 		);
