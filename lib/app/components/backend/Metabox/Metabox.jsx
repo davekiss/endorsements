60a789674a7ce50f1eb3d69d7eab31099862634a
Dave Kiss <dave@davekiss.com>
2017-06-15 11:39:08 -0400

    Refactor Metabox component to ES6 class

--- lib/app/components/backend/Metabox/Metabox.jsx
+++ lib/app/components/backend/Metabox/Metabox.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import Title from 'components/backend/Title/Title.jsx';
@@ -10,13 +10,25 @@ const Wrapper = styled.section`
   background: #ffffff;
 `;
 
-const Metabox = () => {
-  return (
-    <Wrapper>
-      <Title />
-      <Button text="Add New" />
-    </Wrapper>
-  );
-};
+class Metabox extends Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e) {
+		e.preventDefault();
+		console.log('Add a new endorsement here!');
+	}
+
+	render() {
+		return (
+			<Wrapper>
+				<Title />
+				<Button text="Add New" handler={this.handleClick} />
+			</Wrapper>
+		);
+	}
+}
 
 export default Metabox;
